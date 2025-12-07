c1a66d0c2dd334a0ddb506ff1538c9f259e5f48e
Aster <112184370+SystemTrenchcoat@users.noreply.github.com>
2025-12-07 02:34:50 -0500

    Tutorial Debug Underway

--- Assets/Scripts/UI/Tutorial/PlayerTutorialManager.cs
+++ Assets/Scripts/UI/Tutorial/PlayerTutorialManager.cs
@@ -9,13 +9,11 @@ public class Tutorial : MonoBehaviour
     private Vector2 currentPos;
     private Vector2 previousPos;
 
-    private bool sonarUsed = false;
-
     //Timers
     private float tutDelay = 2.4f;
     private float moveTimer = 0;
-    private float sonarTimer = 0;
-    private float releaseTimer = 0;
+    //private float sonarTimer = 0;
+    //private float releaseTimer = 0;
     private float trashTimer = 0;
 
     //Text Variables
@@ -39,20 +37,36 @@ public class Tutorial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Debugging Start
+        Debug.Log("Move: " + moveTut);
+        Debug.Log("Sonar: " + sonarTut);
+        Debug.Log("Release: " + releaseTut);
+        Debug.Log("Trash: " + trashTut);
+        Debug.Log("Trash Info: " + trashInfo);
+        //Debugging End
+
         //Move Tutorial Start
+        previousPos = currentPos;
+        currentPos = transform.position;
+
         if (currentPos != previousPos)
+        {
+            EmptyText();
             moveTut = false;
+        }
 
         else
             moveTimer += Time.deltaTime;
 
-        if (moveTimer >= tutDelay && moveTut)
+        if (moveTut && moveTimer >= tutDelay)
             ShowText("Move your mouse to steer the boat.");
         //Move Tutorial End
 
         //Release Tutorial Start
-        if (releaseTut && gameObject.transform.parent.GetComponent<Boat>().releasedFish)
+        if (releaseTut && gameObject.transform.parent.GetComponent<Boat>().releasedFish){
+            EmptyText();
             releaseTut = false;
+        }
         //Release Tutorial End;
 
         //Trash Tutorial Start
@@ -62,21 +76,11 @@ public class Tutorial : MonoBehaviour
             trashTimer += Time.deltaTime;
 
             if(trashTimer >= tutDelay)
+            {
+                EmptyText();
                 trashInfo = false;
+            }
         }
-    }
-
-    private void ShowText(string text)
-    {
-        if (tutorText == null) return;
-
-        tutorText.text = text;
-
-        Color c = tutorText.color;
-        c.a = textOpacity;
-        tutorText.color = c;
-
-        tutorText.gameObject.SetActive(true);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -88,6 +92,16 @@ public class Tutorial : MonoBehaviour
             if (fish != null) return;
 
             ShowText("Left-click to scare the fish away with sonar.");
+        }
+
+
+        if (sonarTut)
+        {
+            SoundWave sonar = collision.gameObject.GetComponent<SoundWave>();
+            if (sonar != null) return;
+
+            EmptyText();
+            sonarTut = false;
         }
         //Sonar Tutorial End
 
@@ -109,24 +123,33 @@ public class Tutorial : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        //Sonar Tutorial Start
-        if (sonarTut)
-        {
-            SoundWave sonar = collision.gameObject.GetComponent<SoundWave>();
-            if (sonar != null) return;
-
-            sonarTut = false;
-        }
-        //Sonar Tutorial End
-
         //Trash Tutorial Start
         if (trashTut)
         {
             Trash trash = collision.gameObject.GetComponent<Trash>();
             if (trash != null) return;
 
+            EmptyText();
             trashTut = false;
         }
         //Trash Tutorial End
+    }
+
+    private void ShowText(string text)
+    {
+        if (tutorText == null) return;
+
+        tutorText.text = text;
+
+        Color c = tutorText.color;
+        c.a = textOpacity;
+        tutorText.color = c;
+
+        tutorText.gameObject.SetActive(true);
+    }
+
+    private void EmptyText()
+    {
+        ShowText("");
     }
 }
